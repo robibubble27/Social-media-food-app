a71f9c59315dffd97dfaccdec8ee44880c709dc5
robibubble27 <robert.1709@yahoo.com>
2023-01-21 12:16:49 +0200

    first commit

---
+++ src/main/java/com/example/application/data/entity/users/User.java
@@ -0,0 +1,75 @@
+package com.example.application.data.entity;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class User implements Users{
+    @NotEmpty
+    private String firstName = "";
+    @NotEmpty
+    private String lastName = "";
+    @NotEmpty
+    private String username = "";
+    @NotEmpty
+    private String password = "";
+
+    private String description = "";
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Email
+    @NotEmpty
+    private String email = "";
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
